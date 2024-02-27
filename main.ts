1b474821efa114681acfba29dd8ac267344b4ab9
mpoelzl <mpoelzl@redeaglesolutions.com>
2024-02-27 20:45:15 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,51 +1,52 @@
-
+//% weight=100 color=#008C8C block="Easy Cutebot Pro" blockId="Easy Cutebot Pro" icon="\uf48b"
 namespace EasyCbp
 {
-    let i2cAddr: number = 0x10;
+    let forwardSteeringCorrection = 0;
+    let backwardSteeringCorrection = 0;
+    let distanceCorrection = 0;
 
-    enum CutebotProOrientation {
+    enum DriveDirection {
         //%block="forward"
-        Advance = 1,
+        Forward = 1,
         //%block="reverse"
-        Retreat = 0
+        Backward = 0
     }
 
-    enum CutebotProDistanceUnits {
+    enum DistanceUnits {
         //%block="cm"
         Cm = 0,
         //%block="inch"
-        Ft = 1,
+        Inch = 1,
     }
 
-//% group="PID Control"
-//% weight=200
-//% block="go %CutebotProOrientation %distance %CutebotProDistanceUnits"
-    export function myDistanceRunning(orientation: CutebotProOrientation, distance: number, distanceUnits: CutebotProDistanceUnits): void {
-    let buf = pins.createBuffer(7)
-    let curtime = 0
-    let oldtime = 0
-    let tempdistance = 0
-    let temp = 0
-    CutebotPro.pwmCruiseControl(0, 0)
-    if (distanceUnits == CutebotProDistanceUnits.Cm)
-        tempdistance = distance;
-    else if (distanceUnits == CutebotProDistanceUnits.Ft)
-        tempdistance = distance * 0.3937;
+    //% group="Drive Controls"
+    //% weight=20
+    //% block="drive %direction %distance %distanceUnits at speed %speed"
+     //% speed.min=-100 speed.max=100
+    export function driveDistance(direction: DriveDirection, distance: number, distanceUnits: DistanceUnits, speed: number): void {
+        if (distanceUnits == DistanceUnits.Cm)
+            distance = distance;
+        else if (distanceUnits == DistanceUnits.Inch)
+            distance = distance * 2.54;
 
-    if (tempdistance > 3) {
-        temp = Math.floor(tempdistance / 50) + 1
-        tempdistance = tempdistance - temp
-    }
+        let steeringCorrection = forwardSteeringCorrection;
+        let distCorrection = (100 + distanceCorrection) / 100;
+        let targetDegrees = (360 / 158.65) * distance * distCorrection;
 
-    buf[0] = 0x99;
-    buf[1] = 0x03;
-    buf[2] = orientation;
-    buf[3] = tempdistance;
-    buf[4] = 0x00;
-    buf[5] = 0x00;
-    buf[6] = 0x88;
-    pins.i2cWriteBuffer(i2cAddr, buf)
-    basic.pause(tempdistance * 1000 / 20)
-    basic.pause(800)
+        if (direction == DriveDirection.Backward)
+        {
+            steeringCorrection = backwardSteeringCorrection;
+            speed = speed * -1;
+        }
+       
+        CutebotPro.clearWheelTurn(CutebotProMotors1.M1);
+        CutebotPro.pwmCruiseControl(speed + steeringCorrection, speed);
+
+        while(CutebotPro.readDistance(CutebotProMotors1.M1) < targetDegrees)
+        {
+            basic.pause(100);
+        }
+
+        CutebotPro.stopImmediately(CutebotProMotors.ALL);
     }
 }
